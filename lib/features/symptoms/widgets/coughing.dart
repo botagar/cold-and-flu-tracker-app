fab55245c11ba3bf690a253b246b3a874fe56e94
botagar <botagar@gmail.com>
2022-08-10 21:44:50 +1000

    adds some more descriptions for coughing

--- lib/features/symptoms/widgets/coughing.dart
+++ lib/features/symptoms/widgets/coughing.dart
@@ -11,19 +11,19 @@ class CoughingSymptom extends StatelessWidget {
   Widget build(BuildContext context) {
     return Symptom(
       title: "Coughing",
-      defaultExplainerText: "Not experiencing this symptom",
+      defaultExplainerText: "Not experiencing this symptom.",
       explainerText: const {
-        0: "Not experiencing this symptom",
-        1: "Only slight nasal discharge",
-        2: "2",
-        3: "3",
-        4: "4",
-        5: "5",
+        0: "Not experiencing this symptom.",
+        1: "Only light tickle in chest. Not coughing yet.",
+        2: "Light tickle in chest occationally leading to small cough.",
+        3: "Light regular cough.",
+        4: "Occational coughing fits. Slightly out of breath after fits.",
+        5: "Regular coughing fits. Slightly out of breath after fits. Falling asleep disrupted.",
         6: "6",
         7: "7",
-        8: "8",
+        8: "Serveral uncomfortable coughing fits per hour. Out of breath immediately after fits.",
         9: "9",
-        10: "Sinuses are constantly running",
+        10: "Constant painful coughing. Struggling to breath. Light headed after coughing. Sleep is distrupted.",
       },
       onSymptomUpdate: onSymptomUpdated,
     );
